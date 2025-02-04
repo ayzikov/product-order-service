f98f006d594fbea8b0426319b5245ae272a1b02b
RevelRies <ayzikov070@mail.ru>
2025-02-04 22:04:44 +0300

    refactor: test provider views
    minor changes in variable names

--- apps/orderserviceapi/tests/test_views/test_provider_views.py
+++ apps/orderserviceapi/tests/test_views/test_provider_views.py
@@ -13,45 +13,45 @@ class ProviderViewsTest(APITransactionTestCase):
 
     def setUp(self):
         self.url_list_create = reverse("orderserviceapi:providers:list_create")
-        self.url = reverse("orderserviceapi:providers:detail_modify_delete", args=[1])
+        self.url_get_modify_delete = reverse("orderserviceapi:providers:detail_modify_delete", args=[1])
 
     def test_provider_views(self):
         # проверка создания объекта
         for id in range(1, 3):
-            self.provider = ProviderFactory.build()
-            self.response_post = self.client.post(
+            provider = ProviderFactory.build()
+            response_post = self.client.post(
                 path=self.url_list_create,
                 data={
-                    "name": self.provider.name,
-                    "country": self.provider.country,
-                    "town": self.provider.town,
-                    "street": self.provider.street,
-                    "building": self.provider.building
+                    "name": provider.name,
+                    "country": provider.country,
+                    "town": provider.town,
+                    "street": provider.street,
+                    "building": provider.building
                 }
             )
             # проверка статуса ответа
-            self.assertEqual(status.HTTP_201_CREATED, self.response_post.status_code)
+            self.assertEqual(status.HTTP_201_CREATED, response_post.status_code)
             # проверка полей созданного объекта
-            self.url_detail_modify_delete = reverse("orderserviceapi:providers:detail_modify_delete", args=[id])
-            response_get_detail = self.client.get(self.url_detail_modify_delete)
+            self.url_detail = reverse("orderserviceapi:providers:detail_modify_delete", args=[id])
+            response_get_detail = self.client.get(self.url_detail)
 
-            self.assertEqual(self.response_post.data.get("name"), response_get_detail.data.get("name"))
-            self.assertEqual(self.response_post.data.get("country"), response_get_detail.data.get("country"))
-            self.assertEqual(self.response_post.data.get("town"), response_get_detail.data.get("town"))
-            self.assertEqual(self.response_post.data.get("street"), response_get_detail.data.get("street"))
-            self.assertEqual(self.response_post.data.get("building"), response_get_detail.data.get("building"))
+            self.assertEqual(response_post.data.get("name"), response_get_detail.data.get("name"))
+            self.assertEqual(response_post.data.get("country"), response_get_detail.data.get("country"))
+            self.assertEqual(response_post.data.get("town"), response_get_detail.data.get("town"))
+            self.assertEqual(response_post.data.get("street"), response_get_detail.data.get("street"))
+            self.assertEqual(response_post.data.get("building"), response_get_detail.data.get("building"))
 
         # проверка создания 2-х объектов
         response_get_list = self.client.get(self.url_list_create)
         self.assertEqual(len(response_get_list.data), 2)
 
         # проверка изменения объекта
-        self.client.patch(self.url, data={"name": "modified_name"})
+        self.client.patch(self.url_get_modify_delete, data={"name": "modified_name"})
         modified_provider = Provider.objects.get(id=1)
         self.assertEqual(modified_provider.name, "modified_name")
 
         # проверка удаления объекта
-        self.client.delete(self.url)
+        self.client.delete(self.url_get_modify_delete)
         self.assertEqual(Provider.objects.all().count(), 1)
 
 
