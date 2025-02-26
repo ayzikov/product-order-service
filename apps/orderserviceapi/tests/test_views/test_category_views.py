4b199e69cdd66aee70c9948813bd18a5b8890859
RevelRies <ayzikov070@mail.ru>
2025-02-26 15:03:25 +0300

    feat: add catogory tests

---
+++ apps/orderserviceapi/tests/test_views/test_category_views.py
@@ -0,0 +1,51 @@
+# base
+# installed
+from django.urls import reverse
+from rest_framework.test import APITransactionTestCase
+from rest_framework import status
+# local
+from apps.orderserviceapi import selectors
+from apps.orderserviceapi.services import db
+
+
+class OrderViewsTest(APITransactionTestCase):
+    reset_sequences = True
+
+    def setUp(self):
+        """
+        устанавливаем urls
+        """
+        self.url_create = "str"
+
+    def test_category_views(self):
+        """
+        создаем категории (1)
+                          /\
+                         2 3
+                        /
+                       4
+        """
+        # создание категорий
+        response_post_1 = self.client.post(
+            path=self.url_create,
+            data={"name": "main", "parent": 1}
+        )
+        response_post_2 = self.client.post(
+            path=self.url_create,
+            data={"name": "second lvl 1", "parent": 1}
+        )
+        response_post_3 = self.client.post(
+            path=self.url_create,
+            data={"name": "second lvl 2", "parent": 1}
+        )
+        response_post_4 = self.client.post(
+            path=self.url_create,
+            data={"name": "third lvl 1", "parent": 2}
+        )
+
+        # проверяем связи между категориями
+        self.assertEqual("main", selectors.category_get(4).parent.parent.name)
+        self.assertEqual("main", selectors.category_get(3).parent.name)
+
+
+# python manage.py test apps.orderserviceapi.tests.test_views.test_category_views.CategoryViewsTest
